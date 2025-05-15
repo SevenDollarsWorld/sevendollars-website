84313d4eea87487ea9bf84832d8bf8d6aa97584f
SevenDollarsWorld <heyricky81@gmail.com>
2025-05-15 22:16:40 +0800

    remove node_module

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/raporatory-website/',
+  base: '',
   plugins: [react()],
   server: {
     port: 5173
